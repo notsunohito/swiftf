0810d1b18a9333fb38a54ce357c0fac53e3511a6
Shuichiro Kamiya <notsunohito@gmail.com>
2017-11-29 11:12:05 +0900

    Migrate to Swift 4.0

--- Project/swiftfTests/ArrayTest.swift
+++ Project/swiftfTests/ArrayTest.swift
@@ -12,7 +12,7 @@ class ArrayTest: XCTestCase {
     
     func testFlatMap() {
         let r1: [Int] = [[2], [3, 5], [7, 11, 13]].flatMap { $0 } // [2, 3, 5, 7, 11, 13]
-        let r2: [Int] = [1, 2, 3].flatMap { [Int](count: $0, repeatedValue: $0) } // [1, 2, 2, 3, 3, 3]
+        let r2: [Int] = [1, 2, 3].flatMap { [Int](repeating: $0, count: $0) } // [1, 2, 2, 3, 3, 3]
         
         XCTAssertEqual(r1, [2, 3, 5, 7, 11, 13])
         XCTAssertEqual(r2, [1, 2, 2, 3, 3, 3])
@@ -26,6 +26,9 @@ class ArrayTest: XCTestCase {
     func testForEach() {
         let a = [2, 3, 5]
         var i = 0
-        a.forEach { XCTAssertEqual($0, a[i++]) }
+        a.forEach {
+            XCTAssertEqual($0, a[i])
+            i += 1
+        }
     }
 }
